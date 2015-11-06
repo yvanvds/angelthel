1f3cfa07c93ea14217f7ad48bdf52a4879b72516
yvan vander sanden <yvan@attr-x.net>
2015-11-06 21:58:10 +0200

    updated header file

--- angelscript_wrapper.h
+++ angelscript_wrapper.h
@@ -16,3 +16,21 @@
 #include "weakref/weakref.h"
 
 #define AS_ASSERT(r)DYNAMIC_ASSERT(r,"Error");
+
+#define CLASS_DEFINE(name, target) C Str8 CN = name; AS_ASSERT(RegClass<target>(CN, asOBJ_VALUE | asOBJ_POD   ))
+
+#define CLASS_CONSTRUCT(name, construct, destruct) C Str8 CN = name; AS_ASSERT(RegClassBehavior(CN, asBEHAVE_CONSTRUCT, "void f()", asFUNCTION(construct))); AS_ASSERT(RegClassBehavior(CN, asBEHAVE_DESTRUCT, "void f()", asFUNCTION(destruct)))
+
+#define CLASS_COPY(script, native) AS_ASSERT(RegClassBehavior(CN, asBEHAVE_CONSTRUCT, S + "void f" + script, asFUNCTIONPR native))
+
+#define CLASS_FUNC(script, native)       AS_ASSERT(RegClassMethod  (CN, script, asMETHOD   native ))
+#define CLASS_FUNC_ARG(script, native)   AS_ASSERT(RegClassMethod  (CN, script, asMETHODPR native ))
+#define CLASS_PROPERTY(script, native)   AS_ASSERT(RegClassProperty(CN, script, asOFFSET   native ))
+
+#define CLASS_OBJECT(name, target)       AS_ASSERT(RegGlobalProperty(CN + " " + name, &target))
+
+#define CLASS_OPP_EQUALS(script, native) AS_ASSERT(RegClassMethod(CN, S  + "bool opEquals(const " + script + " &in) const", asMETHOD  (native, operator==)))
+#define CLASS_OPP_ASSIGN(script, native) AS_ASSERT(RegClassMethod(CN, CN + " &opAssign(const "    + script + " &in)"      , asMETHODPR(native, operator =, (C native&), native&)))
+
+#define GLOBAL_FUNC(script,native)       AS_ASSERT(RegGlobalFunc(script, asFUNCTION   native, asCALL_CDECL))
+#define GLOBAL_FUNC_ARG(script, native)  AS_ASSERT(RegGlobalFunc(script, asFUNCTIONPR native, asCALL_CDECL))
